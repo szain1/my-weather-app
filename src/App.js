b9ef28b16ae090a80fd0e185cd1c94c26ef83c14
szain1 <talktozainn@gmail.com>
2025-09-16 22:35:13 +0500

    b

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
       1009: <WiCloudy />, // Over
       1030: <WiFog />, // Mist
       1063: <WiRain />, // Patchy rain poss
-      1066: <WiSnow />, // Patchy snow possib
+      1066: <WiSnow />, // Patchy snow poss
       1069: <WiSnow />, // Patchy sleet possib
       1072: <WiRain />, // Patchy freezing driz
       1087: <WiThunderstorm />, // Thundery outbre
